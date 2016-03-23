cf468ed5c8534f1d50d82d64f5c8a1b8021a7028
hm <harsham05@gmail.com>
2016-03-22 22:22:12 -0700

    use absolute paths

--- nutch/dark/dark_chron.sh
+++ nutch/dark/dark_chron.sh
@@ -23,7 +23,7 @@ do
   outfile=$past_day"weap.out"
 
   #cd path/to/dark/script
-  cd ~/darkWeapons/dark_Cron_production
+  cd /usr/local/memex/darkWeapons/dark_Cron_production
   source dark.env
   python es_allSearch.py wea.txt outFiles/$outfile weapDir 2000000 _type $past_day
 
@@ -38,8 +38,8 @@ do
     continue
   fi
 
-  source ~/jdk8.sh
-  cd ~/imagecat/tmp/parser-indexer
-  java -cp target/nutch-tika-solr-1.0-SNAPSHOT.jar edu.usc.cs.ir.cwork.files.DarkDumpPoster -nutch ~/darkWeapons/dark_Cron_production/nutch/runtime/local -solr http://localhost:8983/solr/imagecatdev -timeout 70000 -list ~/darkWeapons/dark_Cron_production/updates/$updates
+  source /usr/local/memex/jdk8.sh
+  cd /usr/local/memex/imagecat/tmp/parser-indexer
+  java -cp target/nutch-tika-solr-1.0-SNAPSHOT.jar edu.usc.cs.ir.cwork.files.DarkDumpPoster -nutch /usr/local/memex/darkWeapons/dark_Cron_production/nutch/runtime/local -solr http://localhost:8983/solr/imagecatdev -timeout 70000 -list /usr/local/memex/darkWeapons/dark_Cron_production/updates/$updates
 done
 echo "JOB COMPLETE"
